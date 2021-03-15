66969f1526bb254b90b8055150acc4fe406eb196
Jiří Špác <capajj@gmail.com>
2021-03-15 13:03:52 +0100

    bump packages, prettify, remove obsolete config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
-    alias: [
-      { find: 'path', replacement: 'path-browserify' },
-    ],
-  },
-  define: {
-    process: {},
-  },
   plugins: [reactRefresh()],
-});
+})
