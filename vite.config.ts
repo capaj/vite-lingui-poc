bf3b9ee5cbab305ce95e6326f7310f0d3cda25be
Jiří Špác <capajj@gmail.com>
2021-04-02 15:58:49 +0200

    add legacy plugin to showcase compatibility with IE

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 const reactBabelRefreshPlugin = require('./reactBabelRefreshPlugin').default
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/' + require('./package.json').name + '/',
-  plugins: [reactBabelRefreshPlugin()],
+  plugins: [
+    reactBabelRefreshPlugin(),
+    legacy({
+      targets: ['defaults', 'not ie <= 10']
+    })
+  ]
 })
